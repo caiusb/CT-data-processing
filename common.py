74ede2f716f490658749fcf594142e5f76f7ccfe
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-05 12:38:47 -0700

    Comment + float cast
    So the result will always be float. I thinkl

--- common.py
+++ common.py
@@ -13,5 +13,10 @@ def readCSV(file):
 			transformations.append(row)
 	return (header, transformations)
 
+'''
+I do the average of the elements of list. I expect
+int or floats. I return a float result, regarless of
+the input type.
+'''
 def average(list):
-	return reduce(lambda x, y: x + y, list)/len(list)
+	return reduce(lambda x, y: x + y, list)/float(len(list))
