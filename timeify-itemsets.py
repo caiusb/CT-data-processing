49ed1efdd1e7f6d47f4145d3ed2a2be94c721c42
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-08-18 14:57:03 -0700

    Added a function to add timestamps instead of the ID's
    More human readable. Still doesn't work. Will have to
    figure out why

---
+++ timeify-itemsets.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import common
+
+atomicTransformationsFile = '../play/test+training-data/atomicTransformations.txt'
+itemSetFolder = '../play/test+training-data/MiningResults/Size'
+
+TRANS_ID = 0
+TIMESTAMP = 2
+
+transformations = {}
+
+
+(header, atomicTrans) = common.readCSV(atomicTransformationsFile)
+
+for line in atomicTrans:
+	transformations[line[TRANS_ID]] = line[TIMESTAMP]
+
+def processItemSetFile(file, t):
+	f = open(file,'r')
+	output = open(file+'inline','w')
+	output.write(f.readLine())
+	output.write(f.readLine())
+	output.write(f.readLine())
+	for line in f:
+		tids = line.split(':')
+		for tid in tids:
+			oneIds = tid.split(',')
+			for id in oneIds:
+				output.write(transformations[id])
+				output.write(',')
+			common.backspace(output)
+			output.write(':')
+		common.backspace(output)
+		output.write('\n')
+	f.close()
+	output.close()
+
+common.traverseMiningResults(itemSetFolder, None, processItemSetFile)
